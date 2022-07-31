1c53723d766f0276d1042323c5aa91a585677775
michaelzick <michaelzick@gmail.com>
2022-07-31 04:49:13 -0700

    Scapy tweaks

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,10 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP()
+    arp_request.pdst =ip
+    print(arp_request.summary())
+    scapy.ls(scapy.ARP())
 
 
-scan('10.0.2.2')
+scan('10.0.2.1/24')
