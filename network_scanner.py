6de26012ded97c10556245201240b7bbd98c495f
michaelzick <michaelzick@gmail.com>
2022-08-01 16:54:46 -0700

    Setting target IP via argument

--- network_scanner.py
+++ network_scanner.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
 
-from numpy import broadcast
 import scapy.all as scapy
+import argparse
 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target',
+                        help='Range of IP targets (ex. 10.1.1.1/24).')
+    (arguments) = parser.parse_args()
 
-def get_arp_request(ip):
-    return scapy.ARP(pdst=ip)
+    if not arguments.target:
+        parser.error('[-] Please specify a target (-t, --target).')
+    return arguments
+
+def get_arp_request(target):
+    return scapy.ARP(pdst=target)
 
 
 def get_broadcast():
     return scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
 
 
-def get_clients_list(ip):
+def get_clients_list(target):
     clients_list = []
-    arp_request = get_arp_request(ip)
+    arp_request = get_arp_request(target)
     broadcast = get_broadcast()
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast,
                               timeout=1, verbose=False)[0]
 
     for answer in answered_list:
-        client_dict = {'ip': answer[1].psrc, 'mac': answer[1].hwsrc}
+        client_dict = {'target': answer[1].psrc, 'mac': answer[1].hwsrc}
         clients_list.append(client_dict)
 
     return clients_list
 
 
-def print_clients_list(ip):
-    clients_list = get_clients_list(ip)
+def print_clients_list():
+    arguments = get_arguments()
+    clients_list = get_clients_list(arguments.target)
 
-    print('IP\t\tMAC Address\n----------------------------------')
+    print('Target IP\tMAC Address\n----------------------------------')
 
     for client in clients_list:
-        print(client['ip'] + '\t' + client['mac'])
+        print(client['target'] + '\t' + client['mac'])
 
 
-print_clients_list('10.0.2.1/24')
+print_clients_list()
