f1542de2e4539fa2797c9a991e5b6c804168d543
michaelzick <michaelzick@gmail.com>
2022-08-01 11:41:16 -0700

    Printing from dict in list

--- network_scanner.py
+++ network_scanner.py
@@ -12,24 +12,28 @@ def get_broadcast():
     return scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
 
 
-def get_answered_list(ip):
+def get_clients_list(ip):
+    clients_list = []
     arp_request = get_arp_request(ip)
     broadcast = get_broadcast()
     arp_request_broadcast = broadcast/arp_request
-    return scapy.srp(arp_request_broadcast,
-                     timeout=1, verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast,
+                              timeout=1, verbose=False)[0]
 
-
-def print_answered_list(ip):
-    answered_list = get_answered_list(ip)
-    clients_list = []
-
-    print('IP\t\tMAC Address\n----------------------------------')
     for answer in answered_list:
         client_dict = {'ip': answer[1].psrc, 'mac': answer[1].hwsrc}
         clients_list.append(client_dict)
 
-    print(clients_list)
+    return clients_list
 
 
-print_answered_list('10.0.2.1/24')
+def print_clients_list(ip):
+    clients_list = get_clients_list(ip)
+
+    print('IP\t\tMAC Address\n----------------------------------')
+
+    for client in clients_list:
+        print(client['ip'] + '\t' + client['mac'])
+
+
+print_clients_list('10.0.2.1/24')
