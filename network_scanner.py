49e27ad5663d1b00657392107c74c36049f6117f
michaelzick <michaelzick@gmail.com>
2022-07-31 18:43:13 -0700

    Refactored

--- network_scanner.py
+++ network_scanner.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
+from numpy import broadcast
 import scapy.all as scapy
 
 
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+def get_arp_request(ip):
+    return scapy.ARP(pdst=ip)
+
+
+def get_broadcast():
+    return scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+
+
+def get_answered_list(ip):
+    arp_request = get_arp_request(ip)
+    broadcast = get_broadcast()
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast,
-                              timeout=1, verbose=False)[0]
+    return scapy.srp(arp_request_broadcast,
+                     timeout=1, verbose=False)[0]
+
+
+def print_answered_list(ip):
+    answered_list = get_answered_list(ip)
 
     print('IP\t\tMAC Address\n----------------------------------')
     for answer in answered_list:
         print(answer[1].psrc + '\t' + answer[1].hwsrc)
 
 
-scan('10.0.2.1/24')
+print_answered_list('10.0.2.1/24')
